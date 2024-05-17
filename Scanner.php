883579622b02848fed11a262b9a3ef9eebe5f4e3
Krasimira Georgieva <krassi77bg@gmail.com>
2024-05-18 00:37:10 +0300

    Created ScannerTests and Scanner classes, added unit tests

---
+++ Scanner.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+class Scanner
+{
+    public function scan(string $productCode): void
+    {
+        // todo
+    }
+
+    public function total(): string
+    {
+        // todo
+    }
+}
---
+++ tests/unit/ScannerTests.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+namespace unit;
+
+require 'Scanner.php';
+
+use PHPUnit\Framework\TestCase;
+use Scanner;
+
+class ScannerTests extends TestCase
+{
+    public function test_total_price_with_special_discount(): void
+    {
+        $scanner = new Scanner();
+        $scanner->scan('#RA');
+        $scanner->scan('#RA');
+        $scanner->scan('#RA');
+        $scanner->scan('#RA');
+        $scanner->scan('#WA');
+        $scanner->scan('#WA');
+        $scanner->scan('#AP');
+        $scanner->scan('#RA');
+
+        $this->assertEquals('15.00', $scanner->total());
+    }
+
+    public function test_total_price_with_special_discount_without_RA(): void
+    {
+        $scanner = new Scanner();
+        $scanner->scan('#WA');
+        $scanner->scan('#AP');
+        $scanner->scan('#WA');
+        $scanner->scan('#WA');
+        $scanner->scan('#WA');
+        $scanner->scan('#AP');
+        $scanner->scan('#AP');
+        $scanner->scan('#WA');
+
+        $this->assertEquals('25.50', $scanner->total());
+    }
+
+    public function test_total_price_with_special_discount_same_number_of_product_type(): void
+    {
+        $scanner = new Scanner();
+        $scanner->scan('#AP');
+        $scanner->scan('#WA');
+        $scanner->scan('#RA');
+        $scanner->scan('#AP');
+        $scanner->scan('#WA');
+        $scanner->scan('#RA');
+        $scanner->scan('#AP');
+        $scanner->scan('#WA');
+        $scanner->scan('#RA');
+
+        $this->assertEquals('25.00', $scanner->total());
+    }
+}
